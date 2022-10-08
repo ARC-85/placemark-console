6d15882bd15c22b6e36f4ea523b88a3852acdfcf
Andrew Cameron <andrewrosscameron@gmail.com>
2022-10-08 01:22:29 +0100

    Lab week 5

--- src/main/kotlin/org/setu/placemark/console/controllers/PlacemarkController.kt
+++ src/main/kotlin/org/setu/placemark/console/controllers/PlacemarkController.kt
@@ -2,13 +2,16 @@ package org.setu.placemark.console.controllers
 
 import mu.KotlinLogging
 import org.setu.placemark.console.main.controller
+import org.setu.placemark.console.models.PlacemarkJSONStore
 import org.setu.placemark.console.models.PlacemarkMemStore
 import org.setu.placemark.console.models.PlacemarkModel
 import org.setu.placemark.console.views.PlacemarkView
 
 class PlacemarkController {
 
-    val placemarks = PlacemarkMemStore()
+    // val placemarks = PlacemarkMemStore()
+
+    val placemarks = PlacemarkJSONStore()
     val placemarkView = PlacemarkView()
     val logger = KotlinLogging.logger {}
 
@@ -16,6 +19,29 @@ class PlacemarkController {
         logger.info { "Launching Placemark Console App" }
         println("Placemark Kotlin App Version 1.0")
 
+    }
+
+    fun start() {
+        logger.info { "Launching Placemark Console App" }
+        println("Placemark Kotlin App Version 1.0")
+
+        var input: Int
+
+        do {
+            input = controller.menu()
+            when(input) {
+                1 -> controller.add()
+                2 -> controller.update()
+                3 -> controller.list()
+                4 -> controller.search()
+                5 -> controller.delete()
+                -99 -> controller.dummyData()
+                -1 -> println("Exiting App")
+                else -> println("Invalid Option")
+            }
+            println()
+        } while (input != -1)
+        logger.info { "Shutting Down Placemark Console App" }
     }
 
     fun menu() :Int { return placemarkView.menu() }
@@ -63,31 +89,25 @@ class PlacemarkController {
         return foundPlacemark
     }
 
+    fun delete() {
+        placemarkView.listPlacemarks(placemarks)
+        var searchId = placemarkView.getId()
+        val aPlacemark = search(searchId)
+
+        if(aPlacemark != null) {
+            placemarks.delete(aPlacemark)
+            println("Placemark Deleted...")
+            placemarkView.listPlacemarks(placemarks)
+        }
+        else
+            println("Placemark Not Deleted...")
+    }
+
     fun dummyData() {
         placemarks.create(PlacemarkModel(title = "New York New York", description = "So Good They Named It Twice"))
         placemarks.create(PlacemarkModel(title= "Ring of Kerry", description = "Some place in the Kingdom"))
         placemarks.create(PlacemarkModel(title = "Waterford City", description = "You get great Blaas Here!!"))
     }
 
-    fun start() {
-        logger.info { "Launching Placemark Console App" }
-        println("Placemark Kotlin App Version 1.0")
 
-        var input: Int
-
-        do {
-            input = controller.menu()
-            when(input) {
-                1 -> controller.add()
-                2 -> controller.update()
-                3 -> controller.list()
-                4 -> controller.search()
-                -99 -> controller.dummyData()
-                -1 -> println("Exiting App")
-                else -> println("Invalid Option")
-            }
-            println()
-        } while (input != -1)
-        logger.info { "Shutting Down Placemark Console App" }
-    }
 }
--- src/main/kotlin/org/setu/placemark/console/main/main.kt
+++ src/main/kotlin/org/setu/placemark/console/main/main.kt
@@ -2,61 +2,16 @@ package org.setu.placemark.console.main
 
 import mu.KotlinLogging
 import org.setu.placemark.console.controllers.PlacemarkController
+import org.setu.placemark.console.models.PlacemarkJSONStore
 import org.setu.placemark.console.models.PlacemarkMemStore
 import org.setu.placemark.console.models.PlacemarkModel
 import org.setu.placemark.console.views.PlacemarkView
 
 private val logger = KotlinLogging.logger {}
 
-val placemarks = PlacemarkMemStore()
-val placemarkView = PlacemarkView()
 val controller = PlacemarkController()
 
 fun main(args: Array<String>) {
     PlacemarkController().start()
 }
 
-fun addPlacemark(){
-    val aPlacemark = PlacemarkModel()
-
-    if (placemarkView.addPlacemarkData(aPlacemark))
-        placemarks.create(aPlacemark)
-    else
-        logger.info("Placemark Not Added")
-}
-
-fun updatePlacemark() {
-
-    placemarkView.listPlacemarks(placemarks)
-    var searchId = placemarkView.getId()
-    val aPlacemark = search(searchId)
-
-    if(aPlacemark != null) {
-        if(placemarkView.updatePlacemarkData(aPlacemark)) {
-            placemarks.update(aPlacemark)
-            placemarkView.showPlacemark(aPlacemark)
-            logger.info("Placemark Updated : [ $aPlacemark ]")
-        }
-        else
-            logger.info("Placemark Not Updated")
-    }
-    else
-        println("Placemark Not Updated...")
-}
-
-fun searchPlacemark() {
-    val aPlacemark = search(placemarkView.getId())!!
-    placemarkView.showPlacemark(aPlacemark)
-}
-
-
-fun search(id: Long) : PlacemarkModel? {
-    var foundPlacemark = placemarks.findOne(id)
-    return foundPlacemark
-}
-
-fun dummyData() {
-    placemarks.create(PlacemarkModel(title = "New York New York", description = "So Good They Named It Twice"))
-    placemarks.create(PlacemarkModel(title= "Ring of Kerry", description = "Some place in the Kingdom"))
-    placemarks.create(PlacemarkModel(title = "Waterford City", description = "You get great Blaas Here!!"))
-}
